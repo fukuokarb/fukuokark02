48f41c3eded80bc6f7aa5fc2bf36b36d728a0c86
horaotoko <horaotoko@gmail.com>
2017-08-14 19:56:19 +0900

    ドロワー作成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,11 @@
-$(function () {
-  $('a[href^=#]').click(function () {
-    var speed = 400,
-      href = $(this).attr("href"),
-      target = $(href === "#" || href === "" ? 'html' : href),
-      position = target.offset().top;
-    $("html, body").animate({scrollTop: position}, speed, "swing");
-    return false;
-  });
+$('a[href^=#]').click(function () {
+  var speed = 400,
+    href = $(this).attr("href"),
+    target = $(href === "#" || href === "" ? 'html' : href),
+    position = target.offset().top;
+  $("html, body").animate({scrollTop: position}, speed, "swing");
+  return false;
+});
+$('.js-drawer-toggle').click(function(){
+  $('.drawer').toggleClass('drawer--closed');
 });
