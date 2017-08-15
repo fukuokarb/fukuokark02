4079182a9f1aa9d05935853e95599fd4f26d45f3
horaotoko <horaotoko@gmail.com>
2017-08-15 14:18:00 +0900

    jsは戻す

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,10 @@
-$('a[href^=#]').click(function () {
-  var speed = 400,
-    href = $(this).attr("href"),
-    target = $(href === "#" || href === "" ? 'html' : href),
-    position = target.offset().top;
-  $("html, body").animate({scrollTop: position}, speed, "swing");
-  return false;
-});
-$('.js-drawer-toggle').click(function(){
-  $('.drawer').toggleClass('drawer--closed');
+$(function () {
+  $('a[href^=#]').click(function () {
+    var speed = 400,
+      href = $(this).attr("href"),
+      target = $(href === "#" || href === "" ? 'html' : href),
+      position = target.offset().top;
+    $("html, body").animate({scrollTop: position}, speed, "swing");
+    return false;
+  });
 });
